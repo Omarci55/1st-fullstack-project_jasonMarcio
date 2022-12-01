a87c45d33d4bfe6c8fc7fad097f6ea0fe9edf203
o.marci@gmx.de <o.marci@gmx.de>
2022-12-01 10:17:06 +0100

    MyContext is set

--- backend-project/app.js
+++ backend-project/app.js
@@ -1,35 +1,52 @@
 import express from "express";
-import morgan from "morgan";
-import mongoose from "mongoose";
+/* import morgan from "morgan";
+import mongoose from "mongoose"; */
+import products from "./data/products.js"
 
 
 //---Initializing the server------------------------------
 const app = express();
-const PORT = process.env.PORT || 12345;
+const PORT = process.env.PORT || 10787;
 //-------------------------------------------
 
 
 
 //---Connecting with server------------------------------
-mongoose.connect(process.env.MONG0_URI, () => {
+/* mongoose.connect(process.env.MONG0_URI, () => {
     console.log("DB connection established!!")
-}) 
+})  */
 
 
 //---Middleware------------------------------
-app.use( morgan("dev") );
+/* app.use( morgan("dev") );
 
-app.use(express.json());
+app.use(express.json()); */
 
 
 
 
 //---Endpoints------------------------------
-app.use("/users", /* upload.single("image"), */ usersRoute);
 
-app.use("/products", /* upload.single("image"), */ productsRoute);
 
-app.use("/orders", ordersRoute);
+app.get("/", (req, res) => {
+    res.send("API running")
+})
+
+app.get("/products", (req, res) => {
+    res.json(products)
+})
+
+app.get("/products/:id", (req, res) => {
+    const product = products.find( item => item._id === req.params.id)
+    res.json(product)
+
+})
+
+/* app.use("/users", upload.single("image"), usersRoute); */
+
+/* app.use("/products", upload.single("image"), productsRoute); */
+
+/* app.use("/orders", ordersRoute); */
 //-------------------------------------------
 
 
--- frontend-project/src/components/context/Container.js
+++ frontend-project/src/components/context/Container.js
@@ -1,63 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { MyContext } from './MyContext';
+import React, { useEffect, useState } from "react";
+import { MyContext } from "./MyContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Container(props) {
 
-    //all this properties are shared within all children
-    const [products, setProducts ] = useState([])
-    const [cart, setCart] = useState([])
-    const [ user, setUser ] = useState(null)
-    const navigate = useNavigate()
+  const [ user, setUser ] = useState([])
+  const [ cart, setCart ] = useState([])
+  const [ products, setProducts ] = useState([])
+  const navigate = useNavigate()
 
-    //this is my GET REQUEST
-    useEffect( () => {
-        //onload event
-        //fetch is a promise, so I have to use .then
-        fetch("/products")
-        .then(res => res.json())
-        .then( result => {
-            console.log(result)
-            setProducts(result)
-        })
+  useEffect( () => {
+    fetch("/products")
+    .then(res => res.json())
+    .then(result => {
+      console.log(result)
+      setProducts(result)
+    })
 
-        const token = localStorage.getItem("token")
-        if(token) {
-          fetch("/users/checkusertoken",
-          {
-            method:"GET",
-            headers:{
-              "token": token
-            }
-          })
-          .then(res => res.json())
-          .then(result => {
-           
-            if(result.success){
-              setUser(result.data)
-            } else {
-              navigate("/login")
-            }
-          })
-        }
-    }, [])
+
+  })
 
   return (
-
-    //Qualquer valor que eu passe no context.provider vai estar disponível para a children
-    <MyContext.Provider 
-      value = { 
-        { 
-          products, setProducts, 
-          cart, setCart, 
-          user, setUser 
-        } 
-      } >
-
-        {props.children}
-
+    <MyContext.Provider
+      value = {
+        {
+          user, setUser,
+          cart, setCart,
+          products, setProducts
+        }
+      }
+    >
+      {props.children}
     </MyContext.Provider>
-    
   )
-
 }
