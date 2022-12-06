94165d717097361174a570d3fd5479ed1541a36c
Jason Lee <jasonw_lee@hotmail.com>
2022-12-06 11:01:16 +0100

    productsschema

--- backend-project/models/productsschema.js
+++ backend-project/models/productsschema.js
@@ -3,5 +3,36 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const productsSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String, 
+        required: true
+    },
+    brand: {
+        type: String, 
+        required: true
+    },
+    price: {
+        type: Number, 
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    img: {
+        type: String, 
+        required: true
+    },
+    inventoryCount: {
+        type: Number, 
+        required: true
+    },
 })
+
+const ProductsCollection = mongoose.model("products", productsSchema);
+
+export default ProductsCollection;
