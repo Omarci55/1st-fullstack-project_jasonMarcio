d522e51f9f1b8783b8681b35129429d6debbff25
Jason Lee <jasonw_lee@hotmail.com>
2022-12-12 11:30:11 +0100

    signup and login

--- frontend-project/src/components/pages/LoginPage.js
+++ frontend-project/src/components/pages/LoginPage.js
@@ -1,49 +1,68 @@
-import React, { useContext, useState } from 'react'
-/* import toast, { Toaster } from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom'; */
-//import { MyContext } from './context/MyContext.js';
-import { Form, Button, Container } from "react-bootstrap"
+import React, { useContext } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
+import { MyContext } from '../context/MyContext'
+import { Form, Button, Container } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
 
-  //const { setUser } = useContext(MyContext)
+  const { setUser } = useContext(MyContext);
+  const navigate = useNavigate();
 
   const loginUser = (e) => {
 
     e.preventDefault()
 
     fetch("/users/login", 
+      {
+        method:"POST",
+        headers:{"Content-Type": "application/json"},
+        body: JSON.stringify(
           {
-            method:"POST",
-            headers:{"Content-Type": "application/json"},
-            body: JSON.stringify(
-              {
-                email: e.target.email.value,
-                password: e.target.password.value
-              }
-            )
-          })
+            email: e.target.email.value,
+            password: e.target.password.value // --> values coming from form
+          }
+        )
+      })
+        .then(res => {
+          const token = res.headers.get("token")
+          localStorage.setItem("token", token)  // --> store token in local storage
+          return res.json() // --> parse json data in the body in res
+        })
+        .then(result => {
+          if(result.success){
+            toast.success("Logged In")
+            setUser(result.data) // --> setUser coming from useContext
+            setTimeout(() => {
+              navigate("/profile")
+            }, 3000)  // --> setTimeout to see message on page
+          } else {
+            toast.success(result.message)
+          }
+        })
   }
 
   return (
 
     <Container >
-    <Form onSubmit={loginUser} className='container-sm'>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+      <Toaster position='top-center'/>
+      <Form onSubmit={loginUser} className='container-sm'>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </Container>
   )
 }
--- frontend-project/src/components/pages/SignupPage.js
+++ frontend-project/src/components/pages/SignupPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Form, Button, Container, Row, Col, InputGroup, /* Toast, FormGroup */ } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
